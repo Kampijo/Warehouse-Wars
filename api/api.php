4b857a19afe2fc9d9453540fc6b1c2f98e1939e7
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-03-05 02:54:26 -0500

    project files

---
+++ api/api.php
@@ -0,0 +1,28 @@
+# Your RESTFUL API
+
+<?php
+$reply = array();
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+	case 'GET':
+		break;
+	case 'PUT':
+		break;
+	case 'POST':
+		break;
+	case 'DELETE':
+		break;
+}
+print json_encode($reply);
+
+
+
+}
+
+
+
+
+
+
+?>
