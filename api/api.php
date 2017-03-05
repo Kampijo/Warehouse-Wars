202dbdc3f174aafa7edce4a944e0c43f825af1dc
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-03-05 16:49:16 -0500

    GET for api

--- api/api.php
+++ api/api.php
@@ -2,10 +2,19 @@
 
 <?php
 $reply = array();
-$method = $_SERVER['REQUEST_METHOD'];
+header('Content-Type: application/json');
+
+$method = $_SERVER['REQUEST_METHOD']; # request method
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/')); # path of request
+$input = json_decode(file_get_contents('php://input'),true); # body of request
+
+$dbconn = pg_connect("host=mcsdb.utm.utoronto.ca dbname=lopeznyg_309 user=lopeznyg password=13779");
 
 switch ($method) {
 	case 'GET':
+		$reply["path"] = $request;
+		$reply["method"] = $method;
+		$reply["input"] = $input;
 		break;
 	case 'PUT':
 		break;
