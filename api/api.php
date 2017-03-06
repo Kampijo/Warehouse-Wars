d67f1deedbc70e71f0fb7609e8231ec7df1685fe
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-03-05 19:46:08 -0500

    rest api, kinda tinkering and figuring out how it works, not getting to work

--- api/api.php
+++ api/api.php
@@ -5,18 +5,20 @@ $reply = array();
 header('Content-Type: application/json');
 
 $method = $_SERVER['REQUEST_METHOD']; # request method
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/')); # path of request
-$input = json_decode(file_get_contents('php://input'),true); # body of request
-
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/')); // for get 
+$input = json_decode(file_get_contents('php://input')); // for post and put request
 $dbconn = pg_connect("host=mcsdb.utm.utoronto.ca dbname=lopeznyg_309 user=lopeznyg password=13779");
 
 switch ($method) {
 	case 'GET':
-		$reply["path"] = $request;
-		$reply["method"] = $method;
-		$reply["input"] = $input;
 		break;
 	case 'PUT':
+		$user = $input["user"];
+		$password = $input["password"];
+		$email = $input["email"];
+		
+		pg_prepare($dbconn, "insertUser", "INSERT INTO user values($1, $2, $3)");
+		pg_execute($dbconn, "insertUser", array($user, $password, $email));
 		break;
 	case 'POST':
 		break;
@@ -24,14 +26,4 @@ switch ($method) {
 		break;
 }
 print json_encode($reply);
-
-
-
-}
-
-
-
-
-
-
 ?>
