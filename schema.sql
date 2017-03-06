76182a9682370a863b6e43ab91de84d117362f3c
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-03-05 19:58:57 -0500

    modified schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,10 @@
-create table user (
+create table appuser (
 	username varchar(255) primary key,
 	password varchar(255),
-	fname varchar(127),
-	lname varchar(127),
 	email varchar(255));
 
-create table hiscores (
-	name varchar(127),
-	hiscore INTEGER,
-	PRIMARY KEY(name,hiscore),
-	FOREIGN KEY(name) REFERENCES user(username));
+create table scores (
+	username varchar(255),
+	score INTEGER,
+	PRIMARY KEY(username,score),
+	FOREIGN KEY(username) REFERENCES appuser(username));
