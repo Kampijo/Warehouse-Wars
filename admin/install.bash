2e4cf82aefbdc95d5a6f66e36deabaed30cfac0b
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-03-07 13:08:38 -0500

    bash script update

--- admin/install.bash
+++ admin/install.bash
@@ -1,3 +1,5 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 read -p "Enter host: " host
 read -p "Enter database name: " dbname
 read -p "Enter UTORid: " utorid
@@ -8,6 +10,7 @@ read -s -p "Enter htaccess password: " htaccesspass
 
 echo -e "\n"
 
+cd "parent_path"
 sed -i "s/hosthere/$host/g" ../api/api.php
 sed -i "s/dbnamehere/$dbname/g" ../api/api.php
 sed -i "s/userhere/$utorid/g" ../api/api.php
@@ -16,5 +19,6 @@ sed -i "s/passwordhere/$password/g" ../api/api.php
 export PGPASSWORD=$password
 psql -h mcsdb.utm.utoronto.ca -d $dbname -U $utorid -f schema.sql
 htpasswd -bms ../htpasswd self $htaccesspass
-path=`pwd`/../htpasswd
+path=`pwd`/../htpasswd 
+echo $path
 sed -i "s/pathhere/$path/g" ../.htaccess
