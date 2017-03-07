850aa9e9d66b5c401b58c580dbbd422bb1391069
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-03-06 20:26:13 -0500

    testing REFACTORED AGAIN

--- api/api.php
+++ api/api.php
@@ -1,4 +1,5 @@
 # Your RESTFUL API
+
 <?php
 	$reply = array();
 	header('Content-Type: application/json');
@@ -6,18 +7,16 @@
 	$method = $_SERVER['REQUEST_METHOD']; # request method
 	$request = explode('/', trim($_SERVER['PATH_INFO'],'/')); // for get 
 	$input = json_decode(file_get_contents('php://input'),true); // for post and put request
-	$dbconn = pg_connect("host=mcsdb.utm.utoronto.ca dbname=lopeznyg_309 user=lopeznyg password=13779");
 	
 	switch ($method) {
 		case 'GET':
 			$type = $request[0]; // if type is user, then access user api
 			$user = $_SERVER["PHP_AUTH_USER"];
 			$pass = $_SERVER["PHP_AUTH_PW"];
-			if($type == "user"){	
-				if($request[1] == $user){
-					// only get information that matches with what is in authorization header 
-					pg_prepare($dbconn, "authorizeUser", "SELECT * FROM appuser WHERE username=$1 and password=$2");
-					$result = pg_execute($dbconn, "authorizeUser", array($user, $pass));
+			if($type == "user"){
+				// only get information that matches with what is in authorization header 
+				$result = authorizeUser($dbconn,$user, $pass);	
+				if($request[1] == $user && count($request) == 2){
 					$row = pg_fetch_array($result);
 					$reply["status"] = ($row == false) ? "Incorrect information entered." : "Success!";
 					if($row != false){
@@ -27,11 +26,11 @@
 					} else {
 						header($_SERVER['SERVER_PROTOCOL']." 401 Unauthorized");
 					}
-				} elseif ($request[1] == $user && $request[2] == "highScores") {
-					pg_prepare($dbconn, "authorizeUser", "SELECT * FROM appuser WHERE username=$1 and password=$2");
-					$result = pg_execute($dbconn, "authorizeUser", array($user, $pass));
+				} else if ($request[1] == $user && request[2] == "highScores") {
 					$row = pg_fetch_array($result);
+					$reply["status"] = ($row == false) ? "Incorrect information entered." : "Success!";
 					if($row != false){
+						// insert specific hiscore query here
 						header($_SERVER['SERVER_PROTOCOL']."200 OK");
 					} else {
 						header($_SERVER['SERVER_PROTOCOL']." 401 Unauthorized");
@@ -43,11 +42,9 @@
 			} else { // if type is hiscores, then return the top 10 scores in database (scores database contains no sensitive information)
 				$result = getHiscores($dbconn);
 				$hiscores = array();
-				$reply["result"]=$result;
 				while($row = pg_fetch_array($result)){
 					$hiscores[] = array("user" => $row["username"], "score" => $row["score"]);
 				}
-		
 				$reply["status"]="Success!";
 				$reply["response"] = $hiscores;
 			}
@@ -58,9 +55,7 @@
 			$email = $input["email"];
 			$type = $input["type"];
 			if($type == "registration"){
-				pg_prepare($dbconn, "insertUser", "INSERT INTO appuser values($1, $2, $3)");
-				$result = pg_execute($dbconn, "insertUser", array($user, $pass, $email));
-							
+				$result = insertUser($dbconn,$user, $email, $pass);
 				$reply["status"] = ($result == false) ? "User already exists!" : "Success!";
 				if($result != false){
 					header($_SERVER['SERVER_PROTOCOL']." 200 OK");
@@ -71,13 +66,10 @@
 				$user = $_SERVER["PHP_AUTH_USER"];
 				$pass = $_SERVER["PHP_AUTH_PW"];
 				$score = $input["score"];
-				pg_prepare($dbconn, "authorizeUser", "SELECT username FROM appuser WHERE username=$1 and password=$2");
-				$result = pg_execute($dbconn, "authorizeUser", array($user, $pass));
+				$result = authorizeUser($dbconn,$user, $pass);
 				$row = pg_fetch_array($result);
 				if($row != false){
-					$reply["score"] = $score;
-					pg_prepare($dbconn, "newScore", "INSERT INTO scores values($1, $2)");
-					pg_execute($dbconn, "newScore", array($user, $score));
+					insertScore($user, $score);
 					header($_SERVER['SERVER_PROTOCOL']." 200 OK");
 				} else {
 					header($_SERVER['SERVER_PROTOCOL']." 401 Unauthorized");
